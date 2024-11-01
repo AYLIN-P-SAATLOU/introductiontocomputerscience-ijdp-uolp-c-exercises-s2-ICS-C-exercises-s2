9cb53c4c700a9064e39ae817267b5c5d598ee769
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-01 20:11:19 +0000

    Exc6Completed

--- ex06.c
+++ ex06.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main() {
+printf("Numbers divisible by 3 are:\n");
+
+    
+    for (int i=100; 50<i; i-- )
+    if (i%3==0)
+    {
+    printf(" %i\n" ,i);
+    
+    }
+    return 0;
+}
