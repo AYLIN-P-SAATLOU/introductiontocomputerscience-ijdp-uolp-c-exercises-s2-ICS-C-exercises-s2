b8368e01f6231b7dfaf8e494777acc5d97cb0a79
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-03 17:35:53 +0300

    Exc7Completed

--- ex07.c
+++ ex07.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(){
+  int matrix[3][3];
+
+  printf("Expected output format:\n ");
+  
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+  
+  for (int i = 0; i < 3; i++){
+    matrix[i][0] = (i + 1) * 5;
+    matrix[i][1] = 100 - ((i) * 10 + 1);    
+    matrix[i][2] = i + 1; 
+  }
+  
+  return 0;
+}
