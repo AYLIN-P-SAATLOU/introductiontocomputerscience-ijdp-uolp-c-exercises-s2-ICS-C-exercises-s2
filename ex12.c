fedd0cc22a8c4e002bb6fdbfeb007f6a874319d3
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-04 20:08:09 +0300

    Exc12 complete

--- ex12.c
+++ ex12.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main() {
+    int number;
+    unsigned long long factorial = 1;
+
+    do {
+        printf("Enter a positive integer less than 50: ");
+        scanf("%d", &number);
+
+        if (number < 0 || number >= 50) {
+            printf("Invalid input. Please enter a positive integer less than 50.\n");
+        }
+    } while (number < 0 || number >= 50);
+
+    
+    printf("%d! = ", number);
+    for (int i = number; i > 0; i--) {
+        printf("%d", i);
+        if (i > 1) {
+            printf(" * ");
+        }
+        factorial *= i;
+    }
+
+    printf(" = %llu\n", factorial);
+
+    return 0;
+}
