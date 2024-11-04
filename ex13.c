a1eb1cc68aa97706fcc1ec86aaa50c7f3e02dbea
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-04 20:12:13 +0300

    Exc13completed

--- ex13.c
+++ ex13.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+void display_factorial_expression(int n) {
+    printf("%d! = ", n);
+    for (int i = n; i > 0; i--) {
+        printf("%d", i);
+        if (i > 1) {
+            printf(" * ");
+        }
+    }
+}
+
+int main() {
+    int number;
+    unsigned long long result;
+
+
+    do {
+        printf("Enter a positive integer less than 50: ");
+        scanf("%d", &number);
+
+        if (number < 0 || number >= 50) {
+            printf("Invalid input. Please enter a positive integer less than 50.\n");
+        }
+    } while (number < 0 || number >= 50);
+    
+    display_factorial_expression(number);
+    
+    result = factorial(number);
+
+    printf(" = %llu\n", result);
+
+    return 0;
+}
