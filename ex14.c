a51d833623ab2dbdc93d441d1e471e9751a56adb
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-15 18:45:35 +0300

    Modified version14

--- ex14.c
+++ ex14.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+    if(n <= 1) return n;
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main() {
     int n;
-
-    
-    printf("Enter the position of Fibonacci sequence to find (n >= 0): ");
+    printf("Enter the position of Fibonacci sequence: ");
     scanf("%d", &n);
 
-    if (n < 0) {
-        printf("Please enter a non-negative integer.\n");
-        return 1; 
-    }
-
+    if (n < 0) return printf("Please enter a non-negative integer.\n"), 1;
     
     printf("Fibonacci sequence up to position %d:\n", n);
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
         printf("%d ", fibonacci(i));
-    }
 
     printf("\n");
-
-    
-    printf("The %d-th Fibonacci number is: %d\n", n, fibonacci(n));
-
     return 0;
 }
