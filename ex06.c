896d51948fbeb573e190eea8c0cdca6d5bd1f6b7
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-15 15:02:40 +0300

    Modified version6

--- ex06.c
+++ ex06.c
@@ -1,14 +1,13 @@
-
 #include<stdio.h>
 
 int main() {
-printf("Numbers divisible by 3 are:\n");
-    
-    for (int i=100; 50<i; i-- )
-    if (i%3==0)
-    {
-    printf(" %i\n" ,i);
-    
+printf("Numbers divisible by 3 from 100 to 50 are:\n");
+
+    for (int i = 100; 50 < i; i-- ){
+         if (i % 3 == 0)
+            {
+            printf(" %i" ,i);
+        }
     }
     return 0;
 }
