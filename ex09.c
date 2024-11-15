505945837f2808bb2e47758b37e4f7e8bcda7cee
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-15 14:49:41 +0300

    Modified version9

--- ex09.c
+++ ex09.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int number, count = 0;
-    int sum = 0;
+    int number, count = 0, sum = 0;
     float average;
 
     printf("Enter numbers (enter -1 to stop):\n");
     
     while (1) {
-        printf("Number: ");
         scanf("%d", &number);
         
-        if (number == -1) {
-            break;
-        }
-
+        if (number == -1) break;
         sum += number;
         count++;
     }
     
-    printf("\nTotal numbers entered (excluding -1): %d\n", count);
-    printf("Sum of numbers: %d\n", sum);
+    printf("\nTotal entry (excluding -1): %d\n", count);
+    printf("Sum: %d\n", sum);
 
     
     if (count > 0) {
-        average = sum / (float)count;
-        printf("Average of numbers: %.2f\n", average);
+        average = (float)sum / count;
+        printf("Average: %.2f\n", average);
     } else {
-        printf("No numbers were entered.\n");
+        printf("No entry detected.\n");
     }
 
     return 0;
