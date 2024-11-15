52e3cb0d498bf017a89272336b36ff46f781b085
240543604-AYLIN POURMOHAMMAD SAATLOU <aylin.prm200079@gmail.com>
2024-11-15 13:00:18 +0300

    Modified version7

--- ex07.c
+++ ex07.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main(){
-  int matrix[3][3];
+  int matrix[10][3];
+
+  for (int i = 0; i < 10; i++){
+    matrix[i][0] = (i + 1) * 5; // first col
+    matrix[i][1] = 100 - ((i) * 10 + 1);  // sec col
+    matrix[i][2] = i + 1;  // third col
+  }
 
   printf("Expected output format:\n ");
   
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d\t", matrix[i][j]);
         }
         printf("\n");
     }
-  
-  for (int i = 0; i < 3; i++){
-    matrix[i][0] = (i + 1) * 5;
-    matrix[i][1] = 100 - ((i) * 10 + 1);    
-    matrix[i][2] = i + 1; 
-  }
   
   return 0;
 }
